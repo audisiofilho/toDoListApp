5c53e1a32d43605ebf0213fe0864e67fe0d438af
audisiofilho <obuzua46@gmail.com>
2022-06-06 14:36:10 -0300

    Iniciando proejeto e fazendo alguns estilos

---
+++ src/pages/Home/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {
+  Container,
+  ControlContainer,
+  DateText,
+  ProfileContainer,
+  ProfileText,
+  FieldText,
+  ControlTille,
+  ControlNumber,
+  ContainerButtons,
+  Button,
+  ButtonText,
+  ContainerIcon,
+  List,
+} from './styles';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import TaskList from '../../components/TaskList';
+
+export default function Home() {
+  const date = new Date().toLocaleDateString();
+
+  const list = [
+    {
+      id: 1,
+      task: 'Fazer compras no mercado',
+    },
+    {
+      id: 2,
+      task: 'Buscar o Julio na escola',
+    },
+    {
+      id: 3,
+      task: 'Fazer compras',
+    },
+  ];
+  return (
+    <Container>
+      <StatusBar backgroundColor="#000" />
+      <ProfileContainer>
+        <ProfileText>Olá, Audisio!</ProfileText>
+        <DateText>{date}</DateText>
+      </ProfileContainer>
+
+      <ControlContainer>
+        <FieldText>
+          <ControlTille>Pendentes:</ControlTille>
+          <ControlNumber>3</ControlNumber>
+        </FieldText>
+        <FieldText>
+          <ControlTille>Concluidas:</ControlTille>
+          <ControlNumber>3</ControlNumber>
+        </FieldText>
+      </ControlContainer>
+
+      <ContainerButtons>
+        <Button>
+          <ContainerIcon>
+            <Icon name="plus" size={25} color="#000" />
+          </ContainerIcon>
+          <ButtonText>Adicionar</ButtonText>
+        </Button>
+        <Button>
+          <ContainerIcon>
+            <Icon name="edit-2" size={25} color="#000" />
+          </ContainerIcon>
+          <ButtonText>Editar</ButtonText>
+        </Button>
+        <Button>
+          <ContainerIcon>
+            <Icon name="trash" size={25} color="#000" />
+          </ContainerIcon>
+          <ButtonText>Excluir</ButtonText>
+        </Button>
+      </ContainerButtons>
+
+      <List
+        data={list}
+        keyExtractor={item => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => <TaskList data={item}/>}
+      />
+    </Container>
+  );
+}
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fafafa;
+`;
+
+export const ProfileContainer = styled.View`
+  height: 135px;
+  background-color: #000;
+  flex-direction: row;
+  padding: 20px;
+  //align-items: center ;
+  justify-content: space-between;
+`;
+export const ProfileText = styled.Text`
+  color: #fff;
+  font-size: 25px;
+  font-weight: bold;
+`;
+export const DateText = styled.Text`
+  color: #fff;
+  font-size: 15px;
+`;
+
+export const ControlContainer = styled.View`
+  background-color: #fff;
+  margin-left: 20px;
+  margin-right: 20px;
+  top: -35px;
+  flex-direction: row;
+  justify-content: space-between;
+  border-radius: 10px;
+`;
+
+export const FieldText = styled.View`
+  padding: 10px;
+`;
+export const ControlTille = styled.Text`
+  color: #000;
+  font-weight: bold;
+  font-size: 15px;
+`;
+export const ControlNumber = styled.Text`
+  text-align: center;
+  color: #000;
+  font-size: 45px;
+`;
+
+export const ContainerButtons = styled.View`
+  padding-left: 40px;
+  padding-right: 40px;
+  //margin: 20px;
+  flex-direction: row;
+  justify-content: space-between ;
+`;
+export const Button = styled.TouchableOpacity`
+    align-items: center ;
+`;
+export const ContainerIcon = styled.View`
+    width:60px ;
+    height:60px ;
+    background-color: #fff;
+    border-radius: 30px;
+    align-items: center ;
+    justify-content: center ;
+`;
+export const ButtonText = styled.Text`
+    color:#000 ;
+    font-size:15px ;
+`;
+
+export const List = styled.FlatList``;
